b405d881ddefd8c650c62b314428e30b06b5c8bc
Andrew Glenn <glennan@oregonstate.edu>
2022-10-06 16:41:58 -0500

    added comments

--- Timers.py
+++ Timers.py
@@ -4,7 +4,7 @@ import time
 
 #create timer function
 def pomodoroTimer():
-    #these lines are just for testing/////////////////////////////
+    #these lines are just for testing///////////////////////////////////
     print('The current time is:', datetime.now())
     #///////////////////////////////////////////////////////////////////
 
@@ -13,12 +13,12 @@ def pomodoroTimer():
         time.sleep(1)
         timer -= 1
     print('Done! Nice Work!')
-    #these lines are just for testing/////////////////////////////
+    #these lines are just for testing///////////////////////////////////
     print('The current time is:', datetime.now())
     #///////////////////////////////////////////////////////////////////
 
 def breakTimer():
-    #these lines are just for testing/////////////////////////////
+    #these lines are just for testing///////////////////////////////////
     print('The current time is:', datetime.now())
     #///////////////////////////////////////////////////////////////////
     timer = (5 * 60)
@@ -26,9 +26,11 @@ def breakTimer():
         time.sleep(1)
         timer -= 1
     print('Your break is over. :( Get back to work!')
-    #these lines are just for testing/////////////////////////////
+    #these lines are just for testing///////////////////////////////////
     print('The current time is:', datetime.now())
     #///////////////////////////////////////////////////////////////////
 
+#also for testing //////////////////////////////////////////////////////
 pomodoroTimer()
 breakTimer()
+#///////////////////////////////////////////////////////////////////////
