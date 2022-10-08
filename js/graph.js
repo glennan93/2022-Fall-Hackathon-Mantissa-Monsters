e6987ee3562cfea273668515e647cc532a937502
rthom9 <thomrobert9@gmail.com>
2022-10-08 11:25:53 -0600

    Before make graph function

--- js/graph.js
+++ js/graph.js
@@ -1,12 +1,15 @@
 const canvasElement = document.getElementById("pomodoro-chart");
 
-const xValues = ["CS161", "CS162", "CS290", "CS261"]; // update with array of courses, get from database
-const yValues = [0, 0, 0, 0]; // update with array of course pomodoros, get from database
-const dates = ["2022-01-01", "2022-02-01", "2022-03-01", "2022-04-01", "2022-05-01", ]; // get from database, list of all dates user has inputted pomodoros for, will be filtered later
+const xValues = ["test1", "test2", "test3", "test4"]; // update with default array of courses for that user from database
+const yValues = [1, 2, 3, 4]; // update with array of course pomodoros
+const dates = ["2022-01-01", "2022-02-01", "2022-03-01", "2022-04-01", "2022-05-01", ]; // list of all dates user has inputted pomodoros for, will be filtered later
 
-const barColors = ['rgb(255, 99, 132, 0.2)', 'rgb(0, 255, 0, 0.2']; // can add more, alternates each bar
+const barColors = ['rgb(255, 99, 132, 0.2)', 'rgb(0, 255, 0, 0.2']; // can add more, color alternates each bar
 const barMarginColors = ['rgb(255, 99, 132)', 'rgb(0, 255, 0)'];
 
+function makeGraph(courses, pomodoros){
+
+}
 const data = {
     labels: xValues,
     datasets: [{
@@ -51,20 +54,21 @@ function clickHandler(evt) {
         const firstPoint = points[0];
         const label = myChart.data.labels[firstPoint.index];
         const value = myChart.data.datasets[firstPoint.datasetIndex].data[firstPoint.index];
-        console.log(label) // name of clicked course. Can be updated with a display function.
+        // console.log(label) // name of clicked course. Can be updated with a display function to eventually
+        // display all the pomodoros for that subject.
     }
 }
 
+// Function that will filter dates of pomodoros to represent in graph based upon user date selection. 
 function filterPomodorosByDate(){
-    const datesCopy = [...dates];
-    console.log(datesCopy);
     const openingDate = new Date(document.getElementById("opening-date").value);
     const closingDate = new Date(document.getElementById("closing-date").value);
     
-    // filteredDates to only include dates between opening-date and closing-date
-    filteredDates = datesCopy.filter((date) => (openingDate.getTime() <= new Date(date).getTime() && new Date(date).getTime() <= closingDate.getTime()));
+    // filteredDates array includes only dates betwen selected date range
+    filteredDates = dates.filter((date) => (openingDate.getTime() <= new Date(date).getTime() && new Date(date).getTime() <= closingDate.getTime()));
 
-    console.log(filteredDates)
+    // I am not sure how to trigger re-render of graph with updated date range.
+    // Re-rendered graph would require the updated pomodoro counts (yValues) for the updated course list (xValues) in that period
 }
 
 canvasElement.onclick = clickHandler;
